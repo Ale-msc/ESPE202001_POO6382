2837bc9614872e7048bbe23e0017a435fc56ca20
Andres Jacome <ajjacome2@espe.edu.ec>
2020-05-13 11:20:37 -0500

    Jacome functions: abs workshop fix c compiler

--- workshops/jacomea/00_C_functions/abs.c
+++ workshops/jacomea/00_C_functions/abs.c
@@ -1,35 +1,21 @@
 #include <stdio.h>
 
-#define _ABS(number) (number > 0 ? number : -number)
-
-double abs(double number);
-float abs(float number);
-int abs(int number);
+#define ABS(number) (number > 0 ? number : -number)
 
 int main() {
     int a = -5;
     float b = -10.5;
     double c = 19.999452;
 
-    printf("abs(-5): %d\n", abs(a));
-    printf("abs(-5): %f\n", abs(b));
-    printf("abs(-5): %lf\n\n\n", abs(c));
+    printf("ABS(%d): %d\n", a, ABS(a));
+    printf("ABS(%f): %f\n", b, ABS(b));
+    printf("ABS(%lf): %lf\n\n\n", c, ABS(c));
+
+    float input;
 
     printf("ingrese un numero: ");
     scanf("%f", &input);
-    printf("abs(%f): %f", input, abs(input));
+    printf("ABS(%f): %f\n", input, ABS(input));
 
     return 0;
-}
-
-int abs(int number) {
-    return _ABS(number);
-}
-
-double abs(double number) {
-    return _ABS(number);
-}
-
-float abs(float number) {
-    return _ABS(number);
 }
